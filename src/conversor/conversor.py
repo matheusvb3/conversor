942e1db21d3f2ef516431c7b716a1f1a2fd2696b
Matheus <matheus.venturini@acad.ufsm.br>
2024-08-19 00:21:50 -0300

    Verifica se o parâmetro passado para as funções de conversão é do tipo esperado

--- src/conversor/conversor.py
+++ src/conversor/conversor.py
@@ -2,16 +2,22 @@ def celsius_para_fahrenheit(temp_celsius: float) -> float:
     """
     Converte a temperatura de Celsius para Fahrenheit.
 
-    :param celsius: Temperatura em Celsius.
+    :param temp_celsius: Temperatura em Celsius.
     :return: Temperatura em Fahrenheit.
+    :raise TypeError: Valor de temperatura não é um inteiro ou float.
     """
+    if not isinstance(temp_celsius, (float, int)):
+        raise TypeError("O valor recebido como parâmetro não é do tipo inteiro ou float.")
     return temp_celsius * 9/5 + 32
 
 def fahrenheit_para_celsius(temp_fahrenheit: float) -> float:
     """
     Converte a temperatura de Fahrenheit para Celsius.
 
-    :param fahrenheit: Temperatura em Fahrenheit.
+    :param temp_fahrenheit: Temperatura em Fahrenheit.
     :return: Temperatura em Celsius.
+    :raise TypeError: Valor de temperatura não é um inteiro ou float.
     """
+    if not isinstance(temp_fahrenheit, (float, int)):
+        raise TypeError("O valor recebido como parâmetro não é do tipo inteiro ou float.")
     return (temp_fahrenheit - 32) * 5/9
--- tests/test_conversor.py
+++ tests/test_conversor.py
@@ -1,11 +1,16 @@
 from conversor.conversor import celsius_para_fahrenheit, fahrenheit_para_celsius
+import pytest
 
 def test_celsius_para_fahrenheit():
     assert celsius_para_fahrenheit(0) == 32
     assert celsius_para_fahrenheit(100) == 212
     assert celsius_para_fahrenheit(-40) == -40
+    with pytest.raises(TypeError):
+        celsius_para_fahrenheit("0")
 
 def test_fahrenheit_para_celsius():
     assert fahrenheit_para_celsius(32) == 0
     assert fahrenheit_para_celsius(212) == 100
     assert fahrenheit_para_celsius(-40) == -40
+    with pytest.raises(TypeError):
+        fahrenheit_para_celsius("0")
