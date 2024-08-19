17f2c7498865351ab5533f9991ecfc1b2905b18f
Matheus <matheus.venturini@acad.ufsm.br>
2024-08-18 23:17:44 -0300

    Atualiza configurações do Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -14,7 +17,10 @@ release = '2024'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc', # Usado para que o Sphinx faça parse das docstrings nos arquivos de código fonte do projeto
+    'myst_parser'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -26,3 +32,5 @@ language = 'pt_BR'
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+sys.path.insert(0, os.path.abspath('../src')) # Informa onde está o diretório contendo o código fonte
